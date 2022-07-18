05d3fbef4eab77943698f29899471c97b64a8f66
sbaumgertner <s.v.baumgertner@gmail.com>
2022-07-18 20:21:21 +0400

    feat: implement reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let str = n.toString();
+  let newStr = '';
+  for (let i = str.length - 1; i >= 0; i--){
+        newStr += str[i];
+  }
+  return parseInt(newStr);
 }
